d39f66675ecde9f79b593dd0b10d957da7890ab7
tangyonghong <tangyonghong@qq.com>
2015-12-29 00:15:13 +0800

    添加路由配置文件

---
+++ app/config/route.php
@@ -0,0 +1,18 @@
+<?php
+
+
+list($a,$b,$c) = explode('.',$_SERVER['HTTP_HOST']);
+define('MAIN_DOMAIN', $a);
+define('DOMAIN_NAME', '.'.$b.'.'.$c);
+
+$wwwDomainName = "www".DOMAIN_NAME;
+
+return array(
+    $wwwDomainName."/" =>'/home/default/index',
+    $wwwDomainName."/news/list.html"=>"/home/news/list",
+    $wwwDomainName."/news/list-<type:\d+>-<page:\d+>.html"=>"/home/news/list", // 长的Url写前面
+    
+    $wwwDomainName."/news/<id:\d+>.html"=>"/home/news/show",
+);
+
+?>
