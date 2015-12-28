eebfc0209f6a7a5aa14db5246eb7c74b306f286a
tangyonghong <tangyonghong@qq.com>
2015-12-29 00:15:45 +0800

    长的路由写前面

--- app/config/route.php
+++ app/config/route.php
@@ -9,9 +9,9 @@ $wwwDomainName = "www".DOMAIN_NAME;
 
 return array(
     $wwwDomainName."/" =>'/home/default/index',
-    $wwwDomainName."/news/list.html"=>"/home/news/list",
-    $wwwDomainName."/news/list-<type:\d+>-<page:\d+>.html"=>"/home/news/list", // 长的Url写前面
     
+    $wwwDomainName."/news/list-<type:\d+>-<page:\d+>.html"=>"/home/news/list", // 长的Url写前面
+    $wwwDomainName."/news/list.html"=>"/home/news/list",
     $wwwDomainName."/news/<id:\d+>.html"=>"/home/news/show",
 );
 
